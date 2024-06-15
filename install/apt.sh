4a40e2cf5be01a0005daa8ffdceddb146ba94e13
shima004 <yellow030408@gmail.com>
2024-06-15 19:27:46 +0900

    Revert "chore: Update apt.sh to install build dependencies and remove unnecessary packages"
    This reverts commit e110ad67a51002d7202823d28a6034d53aa34772.

--- install/apt.sh
+++ install/apt.sh
@@ -4,16 +4,16 @@ source $PWD/install/functions.sh
 
 # install build dependencies
 info_message "Installing build dependencies"
-sudo apt-get install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev -y
+apt-get install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev -y
 
 # install general packages
 info_message "Installing general packages"
-sudo apt-get install curl wget gpg git unzip fontconfig -y
+apt-get install curl wget gpg git unzip fontconfig -y
 
 # install tzdata
 info_message "Installing tzdata"
-DEBIAN_FRONTEND=noninteractive TZ=Asia/Tokyo sudo apt-get install -y tzdata && ln -fs /usr/share/zoneinfo/$TZ /etc/localtime && dpkg-reconfigure --frontend noninteractive tzdata
+DEBIAN_FRONTEND=noninteractive TZ=Asia/Tokyo apt-get install -y tzdata && ln -fs /usr/share/zoneinfo/$TZ /etc/localtime && dpkg-reconfigure --frontend noninteractive tzdata
 
 # install cli tools
 info_message "Installing cli tools"
-sudo apt-get install vim fzf tmux htop -y
+apt-get install vim fzf tmux htop -y
