60fd1eefe037047130c3c04cc5d1d0835951ca2b
shima004 <yellow030408@gmail.com>
2026-05-03 22:16:41 +0900

    feat: zshプラグインの設定を更新し、defer適用を追加

--- home-manager/sheldon.nix
+++ home-manager/sheldon.nix
@@ -6,17 +6,24 @@
     settings = {
       shell = "zsh";
       plugins = {
+        zsh-defer = {
+          github = "romkatv/zsh-defer";
+        };
         zsh-autosuggestions = {
           github = "zsh-users/zsh-autosuggestions";
+          apply = [ "defer" ];
         };
         zsh-syntax-highlighting = {
           github = "zsh-users/zsh-syntax-highlighting";
+          apply = [ "defer" ];
         };
         zsh-history-substring-search = {
           github = "zsh-users/zsh-history-substring-search";
+          apply = [ "defer" ];
         };
         zsh-completions = {
           github = "zsh-users/zsh-completions";
+          apply = [ "defer" ];
         };
       };
     };
